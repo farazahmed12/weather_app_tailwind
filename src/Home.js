86734f73c1e95d1a7482d0f5ac959fed7f9c5a20
Faraz Ahmed <masterdot707@gmail.com>
2023-04-04 00:56:21 +0500

    completed the app

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div>
       <section className="w-full h-screen bg-center bg-cover bg-no-repeat bg-[url('./assets/sunset.jpg')] bg-gray-700 bg-blend-multiply">
-        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 md:py-56">
+        <div className="px-4 mx-auto max-w-screen-xl text-center py-10 md:py-24">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
             Fell free to check Weather around you.
           </h1>
@@ -16,7 +16,7 @@ const Home = () => {
           <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
             <Form setResult={setResult} />
           </div>
-          <div className="flex flex-col justify-between sm:flex-row space-y-4">
+          <div className=" space-y-4">
             <Report result={result} />
           </div>
         </div>
--- src/Report.js
+++ src/Report.js
@@ -1,91 +1,144 @@
 import React from "react";
 
-import { FaTemperatureHigh } from "react-icons/fa";
+import {
+  FaTemperatureHigh,
+  FaCloud,
+  FaEye,
+  FaWind,
+  FaRadiationAlt,
+} from "react-icons/fa";
+import { MdLocationOn } from "react-icons/md";
 import { WiHumidity } from "react-icons/wi";
 
 const Report = ({ result }) => {
-  console.log("result", result && result);
-
-  const current = result.current;
-  const location = result.location;
+  const current = result?.current;
+  const location = result?.location;
 
   return result && Object.keys(result).length > 0 ? (
-    <div className="text-white">
-      {/* Card ! */}
-      <div class="bg-indigo-600 bg-opacity-75 w-full max-w-md p-4 border border-gray-900 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex flex-row justify-evenly gap-0 sm:gap-4 text-white">
+      {/* Card 1 */}
+      <div class="bg-indigo-600 bg-opacity-75 w-full max-w-[7.9rem] sm:max-w-lg p-4 border border-gray-900 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
         <div class="flex items-center justify-between mb-4"></div>
         <div class="flow-root">
           <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
             <li class="py-3 sm:py-4">
               <div class="flex flex-row justify-between items-center space-x-4">
-                <p className="text-lg font-semibold	">Status: </p>
-                <p className="text-3xl font-semibold	">
+                <p className="text-xs sm:text-lg font-semibold	">Status: </p>
+                <p className="text-sm sm:text-2xl font-semibold	">
                   {current.condition.text}
                 </p>
               </div>
             </li>
             <li class="py-3 sm:py-4">
-              <div class="flex flex-row justify-between items-center space-x-4">
-                <p className="text-4xl font-semibold	">
-                  <FaTemperatureHigh />
+              <div class="flex flex-row justify-between items-center space-x-2">
+                <p className="text-xs sm:text-lg font-semibold	">Country: </p>
+                <p className="text-sm sm:text-lg font-semibold	">
+                  {location.country}
                 </p>
-                <p className="text-lg font-semibold	">{`${current.temp_c} °C`}</p>
               </div>
             </li>
             <li class="py-3 sm:py-4">
-              <div class="flex flex-row justify-between items-center space-x-4">
-                <p className="text-4xl font-semibold	">
-                  <FaTemperatureHigh />
+              <div class="flex flex-row justify-between items-center space-x-2">
+                <p className="text-xs sm:text-lg font-semibold	">Latitude: </p>
+                <p className="text-sm sm:text-lg font-semibold	">
+                  {location.lat}
                 </p>
-                <p className="text-lg font-semibold	">{`${current.temp_f} °F`}</p>
               </div>
             </li>
             <li class="py-3 sm:py-4">
-              <div class="flex flex-row justify-between items-center space-x-4">
-                <p className="text-4xl font-semibold	">
-                  <WiHumidity />
+              <div class="flex flex-row justify-between items-center space-x-2">
+                <p className="text-xs sm:text-lg font-semibold	">Longitude: </p>
+                <p className="text-sm sm:text-lg font-semibold	">
+                  {location.lon}
                 </p>
-                <p className="text-lg font-semibold	">{`${current.humidity} %`}</p>
               </div>
             </li>
           </ul>
         </div>
       </div>
-      {/* Card ! */}
-      <div class="bg-indigo-600 bg-opacity-75 w-full max-w-md p-4 border border-gray-900 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      {/* Card 2 */}
+      <div class="bg-indigo-600 bg-opacity-75 w-full max-w-[7.9rem] sm:max-w-lg p-4 border border-gray-900 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
         <div class="flex items-center justify-between mb-4"></div>
         <div class="flow-root">
           <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
             <li class="py-3 sm:py-4">
               <div class="flex flex-row justify-between items-center space-x-4">
-                <p className="text-lg font-semibold	">Status: </p>
-                <p className="text-3xl font-semibold	">
-                  {current.condition.text}
+                <p className="text-3xl sm:text-4xl text-white font-semibold">
+                  <MdLocationOn />
+                </p>
+                <p className="text-sm sm:text-lg font-semibold	">
+                  {location.name}
                 </p>
               </div>
             </li>
             <li class="py-3 sm:py-4">
               <div class="flex flex-row justify-between items-center space-x-4">
-                <p className="text-4xl font-semibold	">
+                <p className="text-3xl sm:text-4xl font-semibold	">
                   <FaTemperatureHigh />
                 </p>
-                <p className="text-lg font-semibold	">{`${current.temp_c} °C`}</p>
+                <p className="text-sm sm:text-lg font-semibold	">{`${current.temp_c} °C`}</p>
               </div>
             </li>
             <li class="py-3 sm:py-4">
               <div class="flex flex-row justify-between items-center space-x-4">
-                <p className="text-4xl font-semibold	">
+                <p className="text-3xl sm:text-4xl font-semibold	">
                   <FaTemperatureHigh />
                 </p>
-                <p className="text-lg font-semibold	">{`${current.temp_f} °F`}</p>
+                <p className="text-sm sm:text-lg font-semibold	">{`${Math.round(
+                  current.temp_f
+                )} °F`}</p>
               </div>
             </li>
             <li class="py-3 sm:py-4">
               <div class="flex flex-row justify-between items-center space-x-4">
-                <p className="text-4xl font-semibold	">
+                <p className="text-3xl sm:text-4xl font-semibold	">
                   <WiHumidity />
                 </p>
-                <p className="text-lg font-semibold	">{`${current.humidity} %`}</p>
+                <p className="text-sm sm:text-lg font-semibold	">{`${current.humidity} %`}</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* Card 3 */}
+      <div class="bg-indigo-600 bg-opacity-75 w-full max-w-[7.9rem] sm:max-w-lg p-4 border border-gray-900 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex items-center justify-between mb-4"></div>
+        <div class="flow-root">
+          <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+            <li class="py-3 sm:py-4">
+              <div class="flex flex-row justify-between items-center space-x-4">
+                <p className="text-3xl sm:text-4xl text-white font-semibold">
+                  <FaCloud />
+                </p>
+                <p className="text-sm sm:text-lg font-semibold	">
+                  {`${current.cloud} %`}
+                </p>
+              </div>
+            </li>
+            <li class="py-3 sm:py-4">
+              <div class="flex flex-row justify-between items-center space-x-8">
+                <p className="text-3xl sm:text-4xl font-semibold	">
+                  <FaEye />
+                </p>
+                <p className="text-sm sm:text-lg font-semibold	">{`${current.vis_km} km`}</p>
+              </div>
+            </li>
+            <li class="py-3 sm:py-4">
+              <div class="flex flex-row justify-between items-center space-x-8">
+                <p className="text-3xl sm:text-4xl font-semibold	">
+                  <FaWind />
+                </p>
+                <p className="text-sm sm:text-lg font-semibold	">{`${Math.round(
+                  current.wind_kph
+                )} kph`}</p>
+              </div>
+            </li>
+            <li class="py-3 sm:py-4">
+              <div class="flex flex-row justify-between items-center space-x-8">
+                <p className="text-3xl sm:text-4xl font-semibold	">
+                  <WiHumidity />
+                </p>
+                <p className="text-sm sm:text-lg font-semibold	">{`${current.humidity} %`}</p>
               </div>
             </li>
           </ul>
